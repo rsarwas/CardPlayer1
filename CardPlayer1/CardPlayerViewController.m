0d91c2b4de10a50d71e02385625e12e7e3ac6b47
Regan Sarwas @ Bob <rsarwas@gmail.com>
2013-06-30 14:11:43 -0800

    Added logic to Flip Text

--- CardPlayer1/CardPlayerViewController.m
+++ CardPlayer1/CardPlayerViewController.m
@@ -11,13 +11,21 @@
 @interface CardPlayerViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *flipCountLabel;
+@property (nonatomic) int flipCount;
 
 @end
 
 @implementation CardPlayerViewController
 
+- (void) setFlipCount:(int)count {
+    _flipCount = count;
+    self.flipCountLabel.text = [NSString stringWithFormat:@"Flips: %d", _flipCount];
+    NSLog(@"Flips: %d", _flipCount);
+}
+
 - (IBAction)flipCard:(UIButton *)sender {
     sender.selected = !sender.isSelected;
+    self.flipCount++;
 }
 
 @end
