2aaebc0067342a3b21d340d19b3a537b129a2294
Regan Sarwas @ Bob <rsarwas@gmail.com>
2013-06-30 13:51:32 -0800

    Added Button Action; removed unused template code

--- CardPlayer1/CardPlayerViewController.m
+++ CardPlayer1/CardPlayerViewController.m
@@ -14,16 +14,8 @@
 
 @implementation CardPlayerViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)flipCard:(UIButton *)sender {
+    sender.selected = !sender.isSelected;
 }
 
 @end
