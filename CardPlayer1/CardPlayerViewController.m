8863064964f043813bbdb579c7309f99cca6e7c3
Regan Sarwas @ Bob <rsarwas@gmail.com>
2013-06-30 16:04:10 -0800

    Added Deck to card flipper

--- CardPlayer1/CardPlayerViewController.m
+++ CardPlayer1/CardPlayerViewController.m
@@ -7,15 +7,23 @@
 //
 
 #import "CardPlayerViewController.h"
+#import "Deck.h"
 
 @interface CardPlayerViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *flipCountLabel;
 @property (nonatomic) int flipCount;
+@property (strong, nonatomic, readonly) Deck *cards;
 
 @end
 
 @implementation CardPlayerViewController
+
+- (Deck *) getCards{
+    if (!_cards)
+        _cards = [[Deck alloc] init];
+    return _cards;
+}
 
 - (void) setFlipCount:(int)count {
     _flipCount = count;
@@ -24,7 +32,12 @@
 }
 
 - (IBAction)flipCard:(UIButton *)sender {
-    sender.selected = !sender.isSelected;
+    if (self.cards.isEmpty) {
+        self.flipCountLabel.text = @"Flips: 52 (Deck is empty)";
+        [sender setTitle:@"" forState:UIControlStateNormal];
+        return;
+    }
+    [sender setTitle:[[self.cards drawRandomCard] description] forState:UIControlStateNormal];
     self.flipCount++;
 }
 
